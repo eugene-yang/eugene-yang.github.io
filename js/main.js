81b618f05a646ad792710a5e6b7baf61d25754b3
Eugene Yang <eugene2528@gmail.com>
2018-10-23 19:31:03 -0400

    first version out

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(function(){
-	
+    // handling bibtex
+    var $template = $('.publication .paper.template');
+    var parser = new BibtexParser();
+    parser.setInput( $('#bibtex').html() );
+    parser.bibtex()
+    var entries = Object.values( parser.getEntries() ).sort( (a,b) => { a['YEAR'] > b['YEAR'] } )
+    entries.forEach( (entry) => {
+        $clone = $template.clone().removeClass('template');
+        entry['TITLE'] = entry['TITLE'].replace('{', '').replace('}', '');
+        
+        ['author', 'title', 'journal', 'year', 'status'].forEach( (k) => {
+            $clone.find( "."+k ).text( entry[ k.toUpperCase() ] )
+        })
+        $clone.find('.pdf').attr('href', entry['HREF'])
+        entry['HREF'] || $clone.find('.pdf').hide()
+        $clone.find('.bib').data('raw', entry['BIBTEXRAW'])
+
+        $clone.insertAfter( $template );
+    } )
+    
+    for( var k in entries ){
+        $clone = $
+    }
+
+    $('#bibtexmodal').on('show.bs.modal', function (event) {
+        var raw = $(event.relatedTarget).data('raw');
+        $(this).find('.raw').html( raw );
+    })
+
 });
