9f139535c55f83e5d9812fb6ea16051bb1cc9658
Eugene Yang <eugene2528@gmail.com>
2021-08-25 12:33:40 -0400

    update

--- js/main.js
+++ js/main.js
@@ -2,32 +2,36 @@ $(function(){
     // handling bibtex
     var $template = $('.publication .paper.template');
     var parser = new BibtexParser();
-    parser.setInput( $('#bibtex').html() );
-    parser.bibtex()
-    var entries = Object.values( parser.getEntries() ).sort( (a,b) => { a['YEAR'] > b['YEAR'] } )
-    entries.forEach( (entry) => {
-        $clone = $template.clone().removeClass('template');
-        entry['HREF'] = entry['URL']
-        entry['JOURNAL'] = entry['JOURNAL'] || entry['BOOKTITLE'] || entry['ARCHIVEPREFIX']
-        entry['TITLE'] = entry['TITLE'].replace('{', '').replace('}', '');
+    // parser.setInput( $('#bibtex').html() );
+    $.get('./pub.bib').done( data => {
+        parser.setInput( data )
+        parser.bibtex()
+        var entries = Object.values( parser.getEntries() ).sort( (a,b) => { a['YEAR'] > b['YEAR'] } )
+        entries.forEach( (entry) => {
+            $clone = $template.clone().removeClass('template');
+            entry['HREF'] = entry['URL']
+            entry['JOURNAL'] = entry['JOURNAL'] || entry['BOOKTITLE'] || entry['ARCHIVEPREFIX']
+            entry['TITLE'] = entry['TITLE'].replace('{', '').replace('}', '');
+            
+            ['author', 'title', 'journal', 'year', 'status'].forEach( (k) => {
+                $clone.find( "."+k ).text( entry[ k.toUpperCase() ] )
+            })
+            $clone.find('.pdf').attr('href', entry['HREF'])
+            entry['HREF'] || $clone.find('.pdf').hide()
+            $clone.find('.bib').data('raw', entry['BIBTEXRAW'])
+
+            $clone.insertAfter( $template );
+        } )
         
-        ['author', 'title', 'journal', 'year', 'status'].forEach( (k) => {
-            $clone.find( "."+k ).text( entry[ k.toUpperCase() ] )
+        for( var k in entries ){
+            $clone = $
+        }
+
+        $('#bibtexmodal').on('show.bs.modal', function (event) {
+            var raw = $(event.relatedTarget).data('raw');
+            $(this).find('.raw').html( raw );
         })
-        $clone.find('.pdf').attr('href', entry['HREF'])
-        entry['HREF'] || $clone.find('.pdf').hide()
-        $clone.find('.bib').data('raw', entry['BIBTEXRAW'])
-
-        $clone.insertAfter( $template );
     } )
-    
-    for( var k in entries ){
-        $clone = $
-    }
 
-    $('#bibtexmodal').on('show.bs.modal', function (event) {
-        var raw = $(event.relatedTarget).data('raw');
-        $(this).find('.raw').html( raw );
-    })
 
 });
